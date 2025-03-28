4fb84d41e5df5e9d89987669b1db09d033ec2548
Amine Kebichi <amkebichi@gmail.com>
2025-03-28 17:15:40 -0400

    initial component development

--- react-app/src/Header.jsx
+++ react-app/src/Header.jsx
@@ -5,7 +5,10 @@ function Header() {
             <h1>my site</h1>
             <nav>
                 <ul>
-                    
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Services</a></li>
+                    <li><a href="#">Contact</a></li>
                 </ul>
             </nav>
         </header>
