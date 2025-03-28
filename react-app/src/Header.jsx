006827b13e22a39a30346dc1c8e28fd2e03086b4
Amine Kebichi <amkebichi@gmail.com>
2025-03-28 16:27:42 -0400

    initial header component

---
+++ react-app/src/Header.jsx
@@ -0,0 +1,15 @@
+function Header() {
+
+    return (
+        <header>
+            <h1>my site</h1>
+            <nav>
+                <ul>
+                    
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Header
